55acae6f38ec9596bd7cb7d007cd68ff44ffbdd0
Kevin Shin <kevnshin@gmail.com>
2014-12-04 22:46:06 -1000

    Completed item counter

--- public/js/todo.js
+++ public/js/todo.js
@@ -1,5 +1,9 @@
 $(function () {
 
+  var total_counter = 0;
+  var completed_counter = 0;
+
+
   $("input#new_todofield").keydown(function (e) {
 
     if(e.keyCode === 13){
@@ -10,18 +14,18 @@ $(function () {
       
         var list_item = $("<li>");
         var list_text = $("<span>");
-      
+        var checkbox = $("<input type='checkbox'>");
+        
+        checkbox.addClass("list_checkbox")
+        list_item.append(checkbox);
         list_item.addClass("list_item");
         list_text.html(text).addClass("list_text");
         list_item.append(list_text);
       
         $("ul.todo_list").append(list_item);
-      
-        var checkbox = $("<input type='checkbox'>");
-        checkbox.addClass("list_checkbox")
+        total_counter++;
+        update_counter(total_counter, completed_counter);
 
-        list_item.prepend("<input type='checkbox'>");
-      
         $(this).val('');
       }
     }
@@ -33,18 +37,27 @@ $(function () {
     if(this.checked){
 
       $(this).siblings("span").first().addClass("strike");
+      completed_counter++;
 
     } else {//unchecked
 
-      console.log("yes");
       $(this).siblings("span").first().removeClass("strike");
-
+      completed_counter--;
     }
+
+    update_counter(total_counter, completed_counter);
 
   });
 
 
+  function update_counter (total, completed) {
 
+    var remaining = total - completed;
+
+    $("span.items_left").html(remaining);
+    $("span.items_completed").html(completed);
+
+  }
 
 
 
