9a8200eedbb90f0a3b99005369a486841aff33b8
Kevin Shin <kevnshin@gmail.com>
2014-12-04 19:56:28 -1000

    Able to append list items from input

--- public/js/todo.js
+++ public/js/todo.js
@@ -1,6 +1,21 @@
 $(function () {
 
+  $("input#new_todofield").keydown(function (e) {
 
+    if(e.keyCode === 13){
+
+      var text = $(this).val();
+      var list_item = $("<li>");
+      list_item.html(text).addClass("list_item");
+      $("ul.todo_list").append(list_item);
+
+      $("this").val().html("");
+
+
+
+    }
+  
+  });
 
 
 
