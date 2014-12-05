21eac030136e92ae7477240440438a9d2c8ab251
Kevin Shin <kevnshin@gmail.com>
2014-12-04 21:32:16 -1000

    Finished checkbox, strikes/unstrikes text on check/uncheck

--- public/js/todo.js
+++ public/js/todo.js
@@ -5,14 +5,47 @@ $(function () {
     if(e.keyCode === 13){
 
       var text = $(this).val();
-      var list_item = $("<li>");
-      list_item.html(text).addClass("list_item");
-      $("ul.todo_list").append(list_item);
-      $(this).val('');
+
+      if(text !== ''){
       
+        var list_item = $("<li>");
+        var list_text = $("<span>");
+      
+        list_item.addClass("list_item");
+        list_text.html(text).addClass("list_text");
+        list_item.append(list_text);
+      
+        $("ul.todo_list").append(list_item);
+      
+        var checkbox = $("<input type='checkbox'>");
+        checkbox.addClass("list_checkbox")
+
+        list_item.prepend("<input type='checkbox'>");
+      
+        $(this).val('');
+      }
     }
-  
   });
+
+
+  $("ul.todo_list").on("click", "input[type='checkbox']", function (e) {
+    
+    if(this.checked){
+
+      $(this).siblings("span").first().wrap("<strike>");
+      //alternative way to do it
+      // $(this).parent().find("span").first().wrap("<strike>");
+
+    } else {//unchecked
+
+      console.log("yes");
+      $(this).siblings("strike").find("span").first().unwrap();
+
+    }
+
+
+  });
+
 
 
 
