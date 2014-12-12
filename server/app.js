824d8d049e6f56765c3f9f1dbe0e486c9f343fd0
Kevin Shin <kevnshin@gmail.com>
2014-12-11 20:55:13 -1000

    Wrote POST data to a file

--- server/app.js
+++ server/app.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var fs = require('fs');
 var app = express();
 
 
@@ -9,7 +10,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 
 app.post('/save', function (req, res) {
-  console.log(req.body);
+  saveTodoList(req.body.list_to_save);
   res.send("success");
 })
 
@@ -21,3 +22,10 @@ var server = app.listen(3000, function () {
   console.log('Example app listening at http://%s:%s', host, port)
 
 })
+
+function saveTodoList (content) {
+  fs.writeFile("./public/todo_save.json", content, function (err) {
+    if(err) return console.log(err);
+    console.log("successfully saved todo_save.json");
+  });
+}
